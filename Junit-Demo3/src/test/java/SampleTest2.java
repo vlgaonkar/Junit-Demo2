5aefa2b1b48f22fcd684e8430aa376cca6b3ceed
vaibhav gaonkar <vlgaonkar@yahoo.com>
2018-06-10 19:14:28 +0530

    hope the issue resolved

--- Junit-Demo3/src/test/java/SampleTest2.java
+++ Junit-Demo3/src/test/java/SampleTest2.java
@@ -8,14 +8,8 @@ public class SampleTest2 {
 	public void SampleTestPass()
 	{
 		int a = 1; 
-<<<<<<< HEAD
 		int b = 5;
-		assertTrue(a * b == 5);/*comment by vaibhav*/
-=======
-		int b = 3;
-		assertTrue(a * b == 3);
->>>>>>> 8235702150c7b41448981c5c8db0c0e0648af3ec
-		
+		assertTrue(a * b == 5);/*comment by vaibhav checking*/		
 	}
 	
 	@Test
