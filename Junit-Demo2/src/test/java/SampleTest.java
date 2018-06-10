14c9787583d405d75e8fbda760db0254d0e0e55f
vaibhav gaonkar <vlgaonkar@yahoo.com>
2018-06-10 17:20:24 +0530

    my first commit

---
+++ Junit-Demo2/src/test/java/SampleTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void SampleTestPass()
+	{
+		int a = 1; 
+		int b = 2;
+		assertTrue(a + b == 3);
+		
+	}
+	
+	@Test
+	public void SampleTestFail()
+	{
+		int a = 1; 
+		int b = 2;
+		assertTrue(b * b == 4);
+	}
+
+}
